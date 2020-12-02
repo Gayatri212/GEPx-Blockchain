152fbc115156a2e25a78e1db05fe985a30856ec6
Gayatri Tagade <tagadegayatri@gmail.com>
2020-12-02 07:40:22 +0000

    Installation commands for pre-req

---
+++ installation.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+sudo yum install docker
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -a -G docker ec2-user
+sudo chmod 666 /var/run/docker.sock
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo yum install git
+curl https://dl.google.com/go/go1.15.2.linux-amd64.tar.gz --output go1.15.2.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.15.2.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+sudo amazon-linux-extras install java-openjdk11
+sudo yum install java-1.8.0-openjdk
+alternatives --config java
+sudo yum install -y gcc-c++ make
+curl -sL https://rpm.nodesource.com/setup_12.x | sudo -E bash -
+sudo yum install -y nodejs
