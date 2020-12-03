0c5eb9fcb03a3850076351d4ea82ce612074f3ec
Gayatri212 <tagadegayatri@gmail.com>
2020-12-03 15:56:07 +0530

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -8,8 +8,6 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-
 sudo yum install git
 curl https://dl.google.com/go/go1.15.2.linux-amd64.tar.gz --output go1.15.2.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.15.2.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
-export GOPATH=/usr/local/go
 sudo chmod 777 $GOPATH/src
 sudo amazon-linux-extras install java-openjdk11
 sudo yum install java-1.8.0-openjdk
@@ -17,3 +15,11 @@ alternatives --config java
 sudo yum install -y gcc-c++ make
 curl -sL https://rpm.nodesource.com/setup_12.x | sudo -E bash -
 sudo yum install -y nodejs
+
+# Set envs permenantly
+# open .bash_profile file using vi editor
+# vi .bash_profile
+# Copy paste following to that file
+# export PATH=$PATH:/usr/local/go/bin
+# export GOPATH=/usr/local/go
+
